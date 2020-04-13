839c13be904a7aa2721a14123d61b9e09569b4df
Carlos Jara <cjjara002@gmail.com>
2020-04-13 12:44:54 -0700

    made nav bar sticky. Needs to be fixed though

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,7 +6,7 @@ class Portfolio extends React.Component {
   state = {
     darkMode: false,
     technology: [
-      { id: 0, tech: "React", image: "https://bit.ly/2QIM7Yo", years: 1 },
+      { id: 0, tech: "React", image: "https://bit.ly/2wChSvl", years: 1 },
       { id: 1, tech: "Javascript", image: "https://bit.ly/39eEoYk", years: 1 },
       { id: 2, tech: "HTML", image: "https://bit.ly/39nPDO1", years: 1 },
       { id: 3, tech: "CSS", image: "https://bit.ly/3bpnLul", years: 1 },
@@ -25,137 +25,151 @@ class Portfolio extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div id="overlay">
-          <div id="EntireBG">
-            <div id="topHead">
+          <div id="overlay">
+            <div id="EntireBG">
+              {/* <div id="topHead"> */}
+              <nav>
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-3">
+                      <ul>
+                        <li>CJ</li>
+                      
+                      <label htmlFor="" className="switch" onClick={this.slider}>
+                      {this.state.darkMode ? (
+                        <input type="checkbox" checked />
+                      ) : (
+                        <input type="checkbox" />
+                      )}
+                      <span className="slider round"></span>
+                    </label>
+                    </ul>
+                    </div>
+                    <div className="col-md-9">
+                      <ul className="navButton">
+                        <li>
+                          {" "}
+                          <a href="k" className="activeButton">
+                            About
+                          </a>
+                        </li>
+                        <li>
+                          <a href="k">Technology</a>
+                        </li>
+                        <li>
+                          <a href="k">Projects</a>
+                        </li>
+                        <li>
+                          <a href="k">Contact</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+              {/* <div id="EntireBG"> */}
+                <div className="row">
+                  <div className="col" id="heroBanner">
+                    <h2>Front End Developer</h2>
+                    <span id="lineBreak"></span>
+                    <h1>CARLOS JARA</h1>
+                    <br />
+                    <div id="sloganContainer">
+                      <em id="testPlus" className="fas fa-angle-double-left"></em>
+                      <h4 className="ImperioH ">
+                        {" "}
+                        Design | Implement | Test | Optimize{" "}
+                      </h4>
+                      <em
+                        id="testPlus"
+                        className="fas fa-angle-double-right"
+                      ></em>
+                    </div>
+                    <br />
+                    {/* <label htmlFor="" className="switch" onClick={this.slider}>
+                      {this.state.darkMode ? (
+                        <input type="checkbox" checked />
+                      ) : (
+                        <input type="checkbox" />
+                      )}
+                      <span className="slider round"></span>
+                    </label> */}
+                  </div>
+                </div>
+              {/* </div> */}
+              {/* </nav> */}
+            </div>
+  
+            <div className="about">
               <div className="container">
                 <div className="row">
-                  <div className="col-md-3">
-                    <ul>
-                      <li>CJ</li>
-                    </ul>
+                  <div className="col-6">
+                    <div className="aboutFt">
+                      <h1>WORKING ON PROGRESS</h1>
+                      <h2>NOT PERFECTION</h2>
+                    </div>
                   </div>
-                  <div className="col-md-9">
-                    <ul className="navButton">
-                      <li>
-                        {" "}
-                        <a href="k" className="activeButton">
-                          About
-                        </a>
-                      </li>
-                      <li>
-                        <a href="k">Technology</a>
-                      </li>
-                      <li>
-                        <a href="k">Projects</a>
-                      </li>
-                      <li>
-                        <a href="k">Contact</a>
-                      </li>
-                    </ul>
+                  <div className="col-6" id="verticalLine">
+                    <p>
+                      <span id="emphasis">I</span> started my career providing
+                      support to patients abroad and domestically. I worked in the
+                      medical field. During that time, I saw the gap that
+                      technology would bridge. I work to provide long term
+                      solutions to real world problems. I also bring with me my
+                      love for art and design. Web Developmet brings both art and
+                      critically thiking into one field. Right now my favorite
+                      technologies are React, JavaScript, CSS , and HTML.
+                    </p>
                   </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col" id="heroBanner">
-                  <h2>Front End Developer</h2>
-                  <span id="lineBreak"></span>
-                  <h1>CARLOS JARA</h1>
-                  <br />
-                  <div id="sloganContainer">
-                    <em id="testPlus" className="fas fa-angle-double-left"></em>
-                    <h4 className="ImperioH ">
-                      {" "}
-                      Design | Implement | Test | Optimize{" "}
-                    </h4>
-                    <em
-                      id="testPlus"
-                      className="fas fa-angle-double-right"
-                    ></em>
-                  </div>
-                  <br />=
-                  <label htmlFor="" className="switch" onClick={this.slider}>
-                    {this.state.darkMode ? (
-                      <input type="checkbox" checked />
-                    ) : (
-                      <input type="checkbox" />
-                    )}
-                    <span className="slider round"></span>
-                  </label>
+                  {/* </div> */}
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="about">
-            <div className="container">
-              <div className="row">
-                <div className="col-6">
-                  <div className="aboutFt">
-                    <h1>WORKING ON PROGRESS</h1>
-                    <h2>NOT PERFECTION</h2>
-                  </div>
-                </div>
-                <div className="col-6" id="verticalLine">
-                  <p>
-                    <span id="emphasis">I</span> started my career providing
-                    support to patients abroad and domestically. I worked in the
-                    medical field. During that time, I saw the gap that
-                    technology would bridge. I work to provide long term
-                    solutions to real world problems. I also bring with me my
-                    love for art and design. Web Developmet brings both art and
-                    critically thiking into one field. Right now my favorite
-                    technologies are React, JavaScript, CSS , and HTML.
-                  </p>
-                </div>
-                {/* </div> */}
-              </div>
-            </div>
-          </div>
-
-          <div className="techUsed">
-            <div className="container">
-              <h1>Technology</h1>
-              <div className="row">
-                
-                
-{this.state.technology.map(tech=> (
-    // <div className="col-sm-6 col-lg-4"> 
-    <div className="col-6">
-    <div className="card-default card" id="boderTech">
-        <div className="row" >
-         <div className="col" >
-           <div className="text-center card-body" >
-             <h4 className="text-muted">{tech.id}</h4>
-             <h5 className="text-uppercase">{tech.tech}</h5>
-             <img
-               className=" mb-2 rounded-circle-thumb32"
-               src={tech.image}
-               alt="Tech"
-               id="techPhoto"
-             />
-             {/* <p>{profile.Description}</p> */}
-             <br />
-             <span>Years: {tech.years}</span>
+  
+            <div className="techUsed">
+              <div className="container">
+                <h1>Technology</h1>
+                <div className="row">
+                  
+                  
+  {this.state.technology.map(tech=> (
+      // <div className="col-sm-6 col-lg-4"> 
+      <div className="col-6">
+      <div className="card-default card" id="boderTech">
+          <div className="row" >
+           <div className="col" >
+             <div className="text-center card-body" >
+               <h4 className="text-muted">{tech.id}</h4>
+               <h5 className="text-uppercase">{tech.tech}</h5>
+               <img
+                 className=" mb-2 rounded-circle-thumb32"
+                 src={tech.image}
+                 alt="Tech"
+                 id="techPhoto"
+               />
+               {/* <p>{profile.Description}</p> */}
+               <br />
+               <span>Years: {tech.years}</span>
+             </div>
+             {/* <div className="d-flex card-footer text-center">
+               <h4>Footer Here</h4>
+             </div> */}
            </div>
-           {/* <div className="d-flex card-footer text-center">
-             <h4>Footer Here</h4>
+           {/* <div className="col-6">
+            <h1> Hello</h1>
            </div> */}
-         </div>
-         {/* <div className="col-6">
-          <h1> Hello</h1>
-         </div> */}
-       </div> 
-     </div>
-     <br/>
-    </div>
-
-
-))}          
-</div>
+         </div> 
+       </div>
+       <br/>
+      </div>
+  
+  
+  ))}          
+  </div>
+              </div>
             </div>
           </div>
-        </div>
+       
       </React.Fragment>
     );
   }

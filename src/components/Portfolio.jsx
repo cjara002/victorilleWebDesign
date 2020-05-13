c8334332f42d00f0b5798540803acf73f4daa23f
Carlos Jara <cjjara002@gmail.com>
2020-05-13 12:32:29 -0700

    made the links work in my project section.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -15,10 +15,39 @@ class Portfolio extends React.Component {
       { id: 6, tech: "Adobe XD", image: "https://bit.ly/39iCbv3", years: 1 },
     ],
     project: [
-      { id: 0, name: "Imperio", image: "https://bit.ly/3amXZpW", stack: "React, HTML, CSS, C#, SQL",},
-      { id: 1, name: "Task Tracker", image: "https://bit.ly/2KmRTeA", stack: "React, HTML, CSS",},
-      { id: 2, name: "YumWise", image: "https://bit.ly/3bluZQy", stack: "HTML, CSS, JavaScript",},
-      { id: 3, name: "AirBnB.Unique", image: "https://bit.ly/2VOgVbG", stack: "React, HTML, CSS, C#, SQL"},
+      {
+        id: 0,
+        name: "Imperio",
+        image: "https://bit.ly/3amXZpW",
+        stack: "React, HTML, CSS, C#, SQL",
+        project: "https://imperiotech.azurewebsites.net/",
+        code: null,
+      },
+      {
+        id: 1,
+        name: "Task Tracker",
+        image: "https://bit.ly/2KmRTeA",
+        stack: "React, HTML, CSS",
+        project: "https://cjara002.github.io/taskTracker/",
+        code: "https://github.com/cjara002/taskTracker",
+        GIFs: "https://media.giphy.com/media/mGcOrK9F3RO9CdfK6L/source.mp4",
+      },
+      {
+        id: 2,
+        name: "YumWise",
+        image: "https://bit.ly/3bluZQy",
+        stack: "HTML, CSS, JavaScript",
+        project: "https://cjara002.github.io/JSQuiz/",
+        code: "https://github.com/cjara002/JSQuiz",
+      },
+      {
+        id: 3,
+        name: "AirBnB.Unique",
+        image: "https://bit.ly/2VOgVbG",
+        stack: "React, HTML, CSS, C#, SQL",
+        project: "https://cjara002.github.io/AirBnB.Unique/",
+        code: "https://github.com/cjara002/AirBnB.Unique",
+      },
     ],
   };
 
@@ -178,51 +207,66 @@ class Portfolio extends React.Component {
             <h1>Projects</h1>
             <div className="container">
               <div className="row">
-                {this.state.project.map(project => (
-                <div className="col-6" key={project.id}>
-                  <div className="card-default card">
-                    
-                    <div className="text-center card-body">
+                {this.state.project.map((project) => (
+                  <div className="col-6" key={project.id}>
+                    <div className="card-default card">
+                      <div className="text-center card-body">
                         <h2>{project.name}</h2>
                         <span>Technology Used: {project.stack}</span>
-                        <br/>
-                      <img
-                        src={project.image}
-                        alt="Contact"
-                        id="profilePhoto"
-                      />
-                    </div>
-                    <div className="card-footer">
-                      <div
-                        className="btn-group btn-group-toggle"
-                        data-toggle="buttons"
-                      >
-                        <label className="btn btn-secondary ">
-                          <input
-                            type="radio"
-                            name="options"
-                          />{" "}
-                          PROJECT
-                        </label>
-                        <label className="btn btn-secondary ">
-                          <input
-                            type="radio"
-                            name="options"
-                          />{" "}
-                          CODE
-                        </label>
-                        <label className="btn btn-secondary">
-                          <input
-                            type="radio"
-                            name="options"
-                          />{" "}
-                          GIFs
-                        </label>
+                        <br />
+                        <img
+                          src={project.image}
+                          alt="Contact"
+                          id="profilePhoto"
+                        />
+                      </div>
+                      <div className="card-footer">
+                        <div
+                          className="btn-group btn-group-toggle"
+                          data-toggle="buttons"
+                        >
+                          <label
+                            className="btn btn-secondary "
+                            // href={project.project}
+                          >
+                            {/* <input type="radio" name="options" /> PROJECT */}
+                            <a
+                              // style={{ decoration: "none" }}
+                              id="projectA"
+                              type="radio"
+                              name="options"
+                              href={project.project}
+                            >
+                              PROJECT
+                            </a>
+                          </label>
+                          <label className="btn btn-secondary ">
+                            <a
+                              // style={{ decoration: "none" }}
+                              id="projectA"
+                              type="radio"
+                              name="options"
+                              href={project.code}
+                            >
+                              {project.code === null ? "N/A" : "CODE "}
+                            </a>
+                          </label>
+                          <label className="btn btn-secondary">
+                            <a
+                              // style={{ decoration: "none" }}
+                              id="projectA"
+                              type="radio"
+                              name="options"
+                              href={project.GIFs}
+                            >
+                              YOUTUBE
+                            </a>
+                          </label>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-               ))}
+                ))}
               </div>
             </div>
           </div>

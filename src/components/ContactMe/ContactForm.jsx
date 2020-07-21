9daa8bafb58ae60b1c55b2db0b5fced0db4f4136
Carlos Jara <cjjara002@gmail.com>
2020-07-21 09:34:04 -0700

    revamped contact form. added links to linkedin and github

--- src/components/ContactMe/ContactForm.jsx
+++ src/components/ContactMe/ContactForm.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import "./ContactStyle.css";
 // import {Form, FormGroup, Label, Button} from "reactstrap"
+import Github from "./Github.png";
+import LinkedIn from "./LinkedIn.png";
 
 class ContactForm extends React.Component {
-  state = {};
+  state = {
+    name: " ",
+    email: " ",
+    message: " "
+  };
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded"},
+      body: encodeURI({ "form-name": "contact", ...this.state})
+    })
+    .then(() =>alert("Success!"))
+    .catch( error => alert(error));
+    e.preventDefault();
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value});
+  }
 
   render() {
+    const {name, email, message } = this.state;
     return (
       <React.Fragment>
        <div style={{backgroundImage: `url(https://bit.ly/2ZqdHy5)`,
@@ -15,18 +37,19 @@ class ContactForm extends React.Component {
                     id="formBorder"
                     >
             <div className="row block-center mt-4 ">
-               <div className="col">
+               <div className="col-6">
                    <div className="container" id="formContainer">
                        <h1 className="formHeader"> Contact Me</h1>
-                        <form name="contact" method="POST" data-netlify="true">
+                        {/* <form name="contact" method="POST" data-netlify="true"> */}
+                        <form onSubmit={this.handleSubmit} name="contact" data-netlify="true">
                           <p>
                             <label className="formName">
-                              Your Full Name: <input type="text" name="name" />
+                              Your Full Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
                             </label>
                           </p>
                           <p>
                             <label className="formEmail">
-                              Your Email: <input type="email" name="email" />
+                              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
                             </label>
                           </p>
                           {/* <p>
@@ -40,7 +63,7 @@ class ContactForm extends React.Component {
                           </p> */}
                           <p>
                             <label className="formMessage">
-                              Message: <textarea name="message" defaultValue={""} />
+                              Message: <textarea name="message" defaultValue={""} value={message} onChange={this.handleChange} />
                             </label>
                           </p>
                           <p>
@@ -48,6 +71,24 @@ class ContactForm extends React.Component {
                           </p>
                         </form>
                    </div>
+               </div>
+               
+               <div className="col-6"> 
+               <ul> 
+                 <li>
+                        <a href="https://github.com/cjara002"> 
+                        
+                        <img src={Github} className="img-responsive" alt="github">
+                          </img>
+                        </a>
+                 </li>
+                 <li>
+                        <a href="www.linkedin.com/in/carlos-j-jara">
+                        <img src={LinkedIn} className="img-responsive" alt="linkedin">
+                          </img>
+                          </a>
+                 </li>
+               </ul>
                </div>
             </div>
        </div>

b859bae3c80969ed19eb60904e77a88cf2d95f59
Carlos Jara <cjjara002@gmail.com>
2020-07-24 12:41:41 -0700

    trying to fix the form section. Should work now

--- src/components/ContactMe/ContactForm.jsx
+++ src/components/ContactMe/ContactForm.jsx
@@ -8,51 +8,70 @@ class ContactForm extends React.Component {
   state = {
     name: " ",
     email: " ",
-    message: " "
+    message: " ",
   };
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     fetch("/", {
       method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded"},
-      body: encodeURI({ "form-name": "contact", ...this.state})
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encodeURI({ "form-name": "contact", ...this.state }),
     })
-    .then(() =>alert("Success!"))
-    .catch( error => alert(error));
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
     e.preventDefault();
-  }
+  };
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value});
-  }
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   render() {
-    const {name, email, message } = this.state;
+    const { name, email, message } = this.state;
     return (
       <React.Fragment>
-       <div style={{backgroundImage: `url(https://bit.ly/2ZqdHy5)`,
-                    backgroundPosition: "center",
-                    // backgroundRepeat: "no-repeat"
-                    }}
-                    id="formBorder"
-                    >
-            <div className="row block-center mt-4 ">
-               <div className="col-6">
-                   <div className="container" id="formContainer">
-                       <h1 className="formHeader"> Contact Me</h1>
-                        {/* <form name="contact" method="POST" data-netlify="true"> */}
-                        <form onSubmit={this.handleSubmit} name="contact" data-netlify="true">
-                          <p>
-                            <label className="formName">
-                              Your Full Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-                            </label>
-                          </p>
-                          <p>
-                            <label className="formEmail">
-                              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-                            </label>
-                          </p>
-                          {/* <p>
+        <div
+          style={{
+            backgroundImage: `url(https://bit.ly/2ZqdHy5)`,
+            backgroundPosition: "center",
+            // backgroundRepeat: "no-repeat"
+          }}
+          id="formBorder"
+        >
+          <div className="row block-center mt-4 ">
+            <div className="col-6">
+              <div className="container" id="formContainer">
+                <h1 className="formHeader"> Contact Me</h1>
+                {/* <form name="contact" method="POST" data-netlify="true"> */}
+                <form
+                  action="POST"
+                  data-netlify="true"
+                  onSubmit={this.handleSubmit}
+                  name="contact"
+                >
+                  <div>
+                    <label className="formName">
+                      Your Full Name:{" "}
+                      <input
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                  </div>
+                  <div>
+                    <label className="formEmail">
+                      Your Email:{" "}
+                      <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                  </div>
+                  {/* <p>
                             <label>
                               Your Role:{" "}
                               <select name="role[]" multiple>
@@ -61,37 +80,53 @@ class ContactForm extends React.Component {
                               </select>
                             </label>
                           </p> */}
-                          <p>
-                            <label className="formMessage">
-                              Message: <textarea name="message" defaultValue={""} value={message} onChange={this.handleChange} />
-                            </label>
-                          </p>
-                          <p>
-                            <button type="submit">Send</button>
-                          </p>
-                        </form>
+                  <div>
+                    <label className="formMessage">
+                      Message:{" "}
+                      <textarea
+                        name="message"
+                        defaultValue={""}
+                        value={message}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                  </div>
+
+                  <div>
+                   <div dtat-netlify-recaptcha="true">
                    </div>
-               </div>
-               
-               <div className="col-6"> 
-               <ul> 
-                 <li>
-                        <a href="https://github.com/cjara002"> 
-                        
-                        <img src={Github} className="img-responsive" alt="github">
-                          </img>
-                        </a>
-                 </li>
-                 <li>
-                        <a href="www.linkedin.com/in/carlos-j-jara">
-                        <img src={LinkedIn} className="img-responsive" alt="linkedin">
-                          </img>
-                          </a>
-                 </li>
-               </ul>
-               </div>
+                  </div>
+                  <div>
+                    <button type="submit">Send</button>
+                  </div>
+                </form>
+              </div>
             </div>
-       </div>
+
+            <div className="col-6">
+              <ul>
+                <li>
+                  <a href="https://github.com/cjara002">
+                    <img
+                      src={Github}
+                      className="img-responsive"
+                      alt="github"
+                    ></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="www.linkedin.com/in/carlos-j-jara">
+                    <img
+                      src={LinkedIn}
+                      className="img-responsive"
+                      alt="linkedin"
+                    ></img>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
